570f8e16f45a495fa8a5904cdff6ddbbbb563e63
Suzzat9 <shobithacherian@Shobithas-MacBook-Air.local>
2024-05-06 14:48:02 -0500

    added select option component to filter bar

--- src/FilterBar.js
+++ src/FilterBar.js
@@ -1,10 +1,31 @@
-import * as React from 'react'
-import TextFilter from './Filters'
+import React, { useState } from 'react'
+import {TextFilter, SelectOption} from './Filters'
 
 const FilterBar = () => {
+
+    const [val, setVal] = useState('');
+
+    const handleChange = (event) => {
+        setVal(event.target.value);
+    };
+
+    const options = [
+        { value: 1, label: '1' },
+        { value: 2, label: '2' },
+        { value: 3, label: '3' },
+        { value: 4, label: '4' },
+        { value: 5, label: '5' },
+        { value: 6, label: '6' },
+        { value: 7, label: '7' },
+        { value: 8, label: '8' },
+        { value: 9, label: '9' },
+        { value: 10, label: '10' },
+    ];
+
     return (
-        <div>
+        <div className="filter-bar">
             <TextFilter className="text-box" filterKey="company" label="Enter company name"/>
+            <SelectOption filterKey="experience" label="Experience" value={val} options={options} onChange={handleChange}/>
         </div>
     )
 }
--- src/Filters.js
+++ src/Filters.js
@@ -1,11 +1,31 @@
 import * as React from 'react'
-import {Box, TextField, Select} from '@mui/material';
+import {Box, TextField, Select, InputLabel, MenuItem, FormControl} from '@mui/material';
 
-const TextFilter = ({ filterKey, label }) => {
+export function TextFilter({ filterKey, label }) {
 
     return (
-        <TextField id = {filterKey} label = {label} className="text-box"/>
+        <TextField id = {filterKey} label = {label} labelId="text-box"/>
     )
 }
 
-export default TextFilter;
+export function SelectOption({ filterKey, label, value, options, onChange }) {
+    
+    return (
+        <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">{label}</InputLabel>
+                <Select
+                labelId="text-box"
+                id={ filterKey }
+                value={value}
+                label={label}
+                onChange={onChange}>
+            {options.map((option) => (
+            <MenuItem key={option.value} value={option.value}>{option.label}</MenuItem>
+            ))}
+            </Select>
+        </FormControl>
+        </Box>
+    );
+
+}
